83aa779a66b45bb5a90ef5c81339b6e39f6b842f
yakugaku22 <haru.aka.aka@gmail.com>
2025-02-10 00:38:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// IndexedDB に音声ファイルを保存
+function saveAudioFile(file) {
+    let request = indexedDB.open("AudioDB", 1);
+
+    request.onupgradeneeded = function(event) {
+        let db = event.target.result;
+        db.createObjectStore("audios", { keyPath: "name" });
+    };
+
+    request.onsuccess = function(event) {
+        let db = event.target.result;
+        let transaction = db.transaction("audios", "readwrite");
+        let store = transaction.objectStore("audios");
+
+        let reader = new FileReader();
+        reader.onload = function() {
+            store.put({ name: file.name, data: reader.result });
+            alert("音声が保存されました！");
+        };
+        reader.readAsDataURL(file);
+    };
+}
+
+// QRコードから取得した音声を再生
+function playAudioFromDB(audioName) {
+    let request = indexedDB.open("AudioDB", 1);
+
+    request.onsuccess = function(event) {
+        let db = event.target.result;
+        let transaction = db.transaction("audios", "readonly");
+        let store = transaction.objectStore("audios");
+
+        let getRequest = store.get(audioName);
+        getRequest.onsuccess = function() {
+            if (getRequest.result) {
+                let audio = document.getElementById("audioPlayer");
+                audio.src = getRequest.result.data;
+                audio.play();
+            } else {
+                alert("音声が見つかりません！");
+            }
+        };
+    };
+}
+
+// ファイル選択ボタンのイベント
+document.getElementById("audioFile").addEventListener("change", function(event) {
+    let file = event.target.files[0];
+    if (file) {
+        saveAudioFile(file);
+    }
+});
+
+//音声読み取り
 document.addEventListener("DOMContentLoaded", function () {
     function onScanSuccess(decodedText) {
         document.getElementById("qr-result").innerText = "QRコード読み取り成功: " + decodedText;
@@ -6,19 +60,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 })
         .render(onScanSuccess);
-});
-
-let audio = new Audio();
-
-// オンラインかオフラインかをチェック
-if (navigator.onLine) {
-    // オンライン時：ネットワークから音声を再生
-    audio.src = "https://qr.me-qr.com/ja/music/q2iR"; // サーバー上の音声URL
-} else {
-    // オフライン時：キャッシュされた音声を再生
-    audio.src = "/ja/music/q2iR";  // PWA内に保存されている音声ファイル
-}
-
-audio.play().catch((error) => {
-    console.error("音声の再生に失敗しました", error);
 });
