9ff233b8f80309dcbbd332d780a499983b84647e
yakugaku22 <haru.aka.aka@gmail.com>
2025-02-10 00:39:27 +0900

    Update server-worker.js

--- server-worker.js
+++ server-worker.js
@@ -1,28 +1,15 @@
-// Service Workerで音声ファイルをキャッシュ
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("audio-cache").then((cache) => {
-            return cache.addAll([
-                "index.html",
-                "script.js",
-                "/ja/music/q2iR" // 音声ファイルをキャッシュ
-            ]);
+        caches.open("qr-audio-cache").then((cache) => {
+            return cache.addAll(["index.html", "script.js"]);
         })
     );
 });
 
-// キャッシュとネットワークの切り替え処理
 self.addEventListener("fetch", (event) => {
-    if (event.request.url.includes("sample.mp3")) {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request).then((fetchResponse) => {
-                    return caches.open("audio-cache").then((cache) => {
-                        cache.put(event.request, fetchResponse.clone());
-                        return fetchResponse;
-                    });
-                });
-            })
-        );
-    }
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
